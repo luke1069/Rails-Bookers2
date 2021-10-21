f4e6675d96980e895b6fc55c1b9c11a970f24e98
luke1069 <luke1069onega14mas@gmail.com>
2021-10-21 14:45:56 +0000

    update1

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   def top
   end
 
-# 　def show
-# 　end
+  def about
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root to:'homes#top'
-  # get'show' => 'homes#show'
+  get 'about' => 'homes#about'
+
   resources :books, only:[:new,:create,:index,:show,:destroy]
   resources :users, only:[:show,:edit,:update,:index]
 end
