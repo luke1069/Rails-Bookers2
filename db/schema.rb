4bed77ca9c2de48dc47d4ee20ffaba65c44b8ec6
luke1069 <luke1069onega14mas@gmail.com>
2021-10-27 01:15:27 +0000

    practice2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_081633) do
+ActiveRecord::Schema.define(version: 2021_10_26_234708) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_10_26_081633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_comment_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
