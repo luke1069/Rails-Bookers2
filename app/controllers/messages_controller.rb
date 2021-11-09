cbc713ad9c8de0d5d15d2a84249cbbc5400917f3
luke1069 <luke1069onega14mas@gmail.com>
2021-11-09 17:18:23 +0000

    DirectMessage3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
+
+  before_action :authenticate_user!, only:[:create]
+
   def create
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = Message.create(message_params)
+      redirect_to room_path(@message.room_id)
+    else
+      flash[:alert] = "メッセージの送信失敗"
+    end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :room_id, :message).merge(user_id: current_user.id)
+  end
+
+
 end
